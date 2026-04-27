ebc9cb3fa5b2a9d5d3f585c62f0b2e751d4e51e2
vincent-silveira <code.vincentsilveira@gmail.com>
2026-04-27 13:34:42 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/desync/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desync.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
